355d0cd1f629eb56395d173fd2e0e652491e5047
Bridget <odanielbri@gmail.com>
2015-01-10 22:16:26 -0500

    Run this file to play the game!

---
+++ Pet_Game_driver.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Pet_Game_driver.py
+# Purpose: To use the Pet_Game class to play with a virtual pet!
+#
+# Author:      odanielb, Bridget O'Daniel
+#
+# Acknowledgements:
+#-------------------------------------------------------------------------------
+
+from Pet_Game import Pet_Game
+
+def main():
+    game = Pet_Game()
+
+if __name__ == '__main__':
+    main()
